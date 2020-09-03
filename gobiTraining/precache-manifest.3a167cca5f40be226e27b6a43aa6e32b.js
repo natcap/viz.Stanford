f7ed90bc8337af2e1d07d30d97732247b98f0801
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2020-09-03 15:23:29 +0200

    Added Gobi Training Viewer v0

---
+++ gobiTraining/precache-manifest.3a167cca5f40be226e27b6a43aa6e32b.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0476c56bdf580d302fc93ad48a8da0",
+    "url": "/gobiTraining/index.html"
+  },
+  {
+    "revision": "4a4e02a88d017d80e835",
+    "url": "/gobiTraining/static/css/2.0b5659ac.chunk.css"
+  },
+  {
+    "revision": "8fe5bf455d2f16c896dd",
+    "url": "/gobiTraining/static/css/main.912ab2fb.chunk.css"
+  },
+  {
+    "revision": "4a4e02a88d017d80e835",
+    "url": "/gobiTraining/static/js/2.d899a3d4.chunk.js"
+  },
+  {
+    "revision": "11f86ddc566c30b6cad223d8a078311f",
+    "url": "/gobiTraining/static/js/2.d899a3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe5bf455d2f16c896dd",
+    "url": "/gobiTraining/static/js/main.14ad4960.chunk.js"
+  },
+  {
+    "revision": "c16dad34e7c8488fb18c",
+    "url": "/gobiTraining/static/js/runtime-main.5a336880.js"
+  },
+  {
+    "revision": "f8041b0394cc02fa9256be370287c198",
+    "url": "/gobiTraining/static/media/Watersheds.f8041b03.zip"
+  },
+  {
+    "revision": "b6dc3a8edb61ce4560ec1efbab7ed778",
+    "url": "/gobiTraining/static/media/accessibility.b6dc3a8e.zip"
+  },
+  {
+    "revision": "a831c7165bb54c334bbed2ff05b76a89",
+    "url": "/gobiTraining/static/media/af.a831c716.png"
+  },
+  {
+    "revision": "20f19f9e5a7f8298bb041346bf62e25d",
+    "url": "/gobiTraining/static/media/airplane.20f19f9e.svg"
+  },
+  {
+    "revision": "3923a11bafb37887c5433283e9a9ca39",
+    "url": "/gobiTraining/static/media/all_ag_c.3923a11b.tif"
+  },
+  {
+    "revision": "326224fa9eb1d8f4971505a24111aac1",
+    "url": "/gobiTraining/static/media/all_road_c.326224fa.tif"
+  },
+  {
+    "revision": "c13fa0899ff1182ab56946d9f8d2463f",
+    "url": "/gobiTraining/static/media/aoi.c13fa089.zip"
+  },
+  {
+    "revision": "8234535842893c99bac6b47b1e85922a",
+    "url": "/gobiTraining/static/media/aoi_gcs.82345358.zip"
+  },
+  {
+    "revision": "a7e19508c6f669a56d4eb294fd5c9a13",
+    "url": "/gobiTraining/static/media/carbon_sample_biophysical_table.a7e19508.csv"
+  },
+  {
+    "revision": "db2bf4cd8f729b58f914c26c7ae70489",
+    "url": "/gobiTraining/static/media/city.db2bf4cd.svg"
+  },
+  {
+    "revision": "32406ab8abb5edf5820cf907b0d1a799",
+    "url": "/gobiTraining/static/media/cw.32406ab8.png"
+  },
+  {
+    "revision": "a92836372bfc0bfbe07a48a5627e01f1",
+    "url": "/gobiTraining/static/media/doubleLeaf.a9283637.svg"
+  },
+  {
+    "revision": "f9068c05277360aad1a5245e86e3aa2a",
+    "url": "/gobiTraining/static/media/exampleLegend4.f9068c05.png"
+  },
+  {
+    "revision": "8ce20f1f4ff4c5658bd9c1fa5075dcbe",
+    "url": "/gobiTraining/static/media/forest.8ce20f1f.svg"
+  },
+  {
+    "revision": "204d8c99c44947d44b272611e6e5333e",
+    "url": "/gobiTraining/static/media/ginger_kowal.204d8c99.jpg"
+  },
+  {
+    "revision": "80af5a47c680a6ed1c2f49911a15783e",
+    "url": "/gobiTraining/static/media/headerImg.80af5a47.png"
+  },
+  {
+    "revision": "d06dac61eed3751bea38e2463369ef19",
+    "url": "/gobiTraining/static/media/herders_c.d06dac61.tif"
+  },
+  {
+    "revision": "c93029b43342d911cae789df919c8d2b",
+    "url": "/gobiTraining/static/media/herders_legend.c93029b4.png"
+  },
+  {
+    "revision": "6fac5a756d01617d54eec174a96366dc",
+    "url": "/gobiTraining/static/media/house.6fac5a75.svg"
+  },
+  {
+    "revision": "2a59328c6e4f13228b260877f8e45e08",
+    "url": "/gobiTraining/static/media/hqm_sensitivity.2a59328c.csv"
+  },
+  {
+    "revision": "83463d4d6db4e41030eb3d27d734cdd6",
+    "url": "/gobiTraining/static/media/hqm_threats.83463d4d.csv"
+  },
+  {
+    "revision": "a231634d338565d00c0e2e1101903e22",
+    "url": "/gobiTraining/static/media/jade.a231634d.png"
+  },
+  {
+    "revision": "630971fb41a0fe2f816a295bf9eff547",
+    "url": "/gobiTraining/static/media/leaf.630971fb.svg"
+  },
+  {
+    "revision": "039f3890df473d550dae727b09c360b2",
+    "url": "/gobiTraining/static/media/legend.039f3890.psd"
+  },
+  {
+    "revision": "43199da8e154bb7ce54de93806b954fd",
+    "url": "/gobiTraining/static/media/lori-avila.43199da8.jpg"
+  },
+  {
+    "revision": "03328fd674ad740a90f62b584e790906",
+    "url": "/gobiTraining/static/media/lulc_legend.03328fd6.png"
+  },
+  {
+    "revision": "775444ac9acf6d60cc3fcc20f7590683",
+    "url": "/gobiTraining/static/media/lulc_modis.775444ac.tif"
+  },
+  {
+    "revision": "7480014e8ef4c6e536712268564b62f3",
+    "url": "/gobiTraining/static/media/mine_c.7480014e.tif"
+  },
+  {
+    "revision": "c6a9c80e61161204827462d8e1213897",
+    "url": "/gobiTraining/static/media/natcap.c6a9c80e.png"
+  },
+  {
+    "revision": "8ac0faef57d3116b718290f220060876",
+    "url": "/gobiTraining/static/media/nationalParks.8ac0faef.zip"
+  },
+  {
+    "revision": "549645571db759100883ff6efc7410a8",
+    "url": "/gobiTraining/static/media/nightlight_c.54964557.tif"
+  },
+  {
+    "revision": "6f4cfa444485bb3619b249d5805799ca",
+    "url": "/gobiTraining/static/media/nightlight_legend.6f4cfa44.png"
+  },
+  {
+    "revision": "c2ce20e8789eac9eb06bff205a02bb51",
+    "url": "/gobiTraining/static/media/no-profile-img.c2ce20e8.png"
+  },
+  {
+    "revision": "1c665bf81fc162bdd7e3b4b1fee76f99",
+    "url": "/gobiTraining/static/media/oggie.1c665bf8.jpg"
+  },
+  {
+    "revision": "105d883d3433f9f3c53634f3d24018b9",
+    "url": "/gobiTraining/static/media/railways_c.105d883d.tif"
+  },
+  {
+    "revision": "75142d765447eb55a2ac8855121714ee",
+    "url": "/gobiTraining/static/media/urban_c.75142d76.tif"
+  }
+]);
